ea8131bb8d454cf54af063871e7fb6fc36199e3f
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-06-11 12:58:12 +0530

    implemented singleton pattern

---
+++ Creational/Singleton Pattern/example_01/com/harshan92/Demo.java
@@ -0,0 +1,9 @@
+package com.harshan92;
+
+public class Demo {
+    public static void main(String[] args) {
+        SingleObject object=SingleObject.getInstance();
+
+        object.showMessage();
+    }
+}

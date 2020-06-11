d75e9b160b5b83da6417eed7e25836e8db28d801
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-06-11 14:29:05 +0530

    implemented builder pattern

---
+++ Creational/Builder Pattern/Meal Example/com/harshan92/Coke.java
@@ -0,0 +1,15 @@
+package com.harshan92;
+
+public class Coke extends ColdDrink{
+
+    @Override
+    public String name() {
+        return "Coke";
+    }
+
+    @Override
+    public float price() {
+        return 12f;
+    }
+    
+}
---
+++ Creational/Builder Pattern/Meal Example/com/harshan92/Wrapper.java
@@ -0,0 +1,10 @@
+package com.harshan92;
+
+public class Wrapper implements Packing{
+
+    @Override
+    public String pack() {
+        return "Wrapper";
+    }
+    
+}

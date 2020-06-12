4992473741707bba58521744a484c2310d86dbb9
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-06-12 19:28:01 +0530

    implemented Facade pattern

---
+++ Structural/Facade Pattern/Shape Maker Example/com/harshan92/Demo.java
@@ -0,0 +1,11 @@
+package com.harshan92;
+
+public class Demo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker=new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawCircle();
+        shapeMaker.drawSquare();
+    }
+}
